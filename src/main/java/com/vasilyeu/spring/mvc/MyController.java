5f4fde4334a2296cfaa066c7a7bdd952ec70b146
vasiljeu95 <vasilyeu95@gmail.com>
2022-06-27 20:25:10 +0200

    annotation @RequestMapping for Controller

--- src/main/java/com/vasilyeu/spring/mvc/MyController.java
+++ src/main/java/com/vasilyeu/spring/mvc/MyController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * @since 26.06.2022
  */
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping("/")
     public String showFirstView () {
@@ -24,21 +25,6 @@ public class MyController {
     public String addEmployeeDetails() {
         return "askEmployeeDetailsView";
     }
-
-//    @RequestMapping("showDetails")
-//    public String showEmployeeDetails() {
-//        return "showEmployeeDetailsView";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("description", " - udemy person");
-//
-//        return "showEmployeeDetailsView";
-//    }
 
     @RequestMapping("/showDetails")
     public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
